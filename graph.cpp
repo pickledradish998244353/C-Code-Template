e90acde5d425773f88500315b68d54ae435b9e26
pickledradish998244353 <1249046060@qq.com>
2026-04-04 15:43:12 +0800

    最短路

---
+++ graph.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+
+#define x first
+#define y second
+#define all(x) x.begin(), x.end()
+#define vec1(T, name, n, val) vector<T> name(n, val)
+#define vec2(T, name, n, m, val) vector<vector<T>> name(n, vector<T>(m, val))
+#define vec3(T, name, n, m, k, val) vector<vector<vector<T>>> name(n, vector<vector<T>>(m, vector<T>(k, val)))
+#define vec4(T, name, n, m, k, p, val) vector<vector<vector<vector<T>>>> name((n), vector<vector<vector<T>>>((m), vector<vector<T>>((k), vector<T>((p), (val)))))
+
+using namespace std;
+using i128 = __int128;
+using u128 = unsigned __int128;
+using LL = long long;
+using LD = long double;
+using ULL = unsigned long long;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+using PLD = pair<LD, LD>;
+
+const int N = 1e5 + 10, MOD = 998244353;
+const int INF = 1e9;
+const LL LL_INF = 2e18;
+const LD EPS = 1e-8;
+const int dx4[] = {-1, 0, 1, 0}, dy4[] = {0, 1, 0, -1};
+const int dx8[] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy8[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+istream& operator>>(istream& is, i128& val) {
+    string str;
+    is >> str;
+    val = 0;
+    bool flag = false;
+    if (str[0] == '-') flag = true, str = str.substr(1);
+    for (char& c : str) val = val * 10 + c - '0';
+    if (flag) val = -val;
+    return is;
+}
+
+ostream& operator<<(ostream& os, i128 val) {
+    if (val < 0) os << "-", val = -val;
+    if (val > 9) os << val / 10;
+    os << static_cast<char>(val % 10 + '0');
+    return os;
+}
+
+bool cmp(LD a, LD b) {
+    if (fabs(a - b) < EPS) return 1;
+    return 0;
+}
+
+LL qpow(LL a, LL b) {
+    LL ans = 1;
+    a %= MOD;
+    while (b) {
+        if (b & 1) ans = ans * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+auto dijkstra(int n, int st, vector<vector<PII>> &g) {
+    vector<bool> vis(n + 1);
+    vector<LL> d(n + 1, LL_INF);
+    d[st] = 0;
+    priority_queue<PLL, vector<PLL>, greater<LL>> q;
+    q.push({0, st});
+
+    while (q.size()) {
+        auto [x, y] = q.top();
+        q.pop();
+        if (vis[y]) continue;
+        vis[y] = 1;
+        for (auto &[v, w] : g[y]) {
+            if (d[v] != LL_INF && d[v] + w < d[y]) {
+                d[y] = d[v] + w;
+                q.push({d[v], v});
+            }
+         }
+    }
+
+    return d;
+}
+
+void solve() {
+    int n;
+    vector<vector<PII>> g(n + 1);
+/**/ #ifdef LOCAL
+    cout << flush;
+/**/ #endif
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int T = 1;
+    while (T--) solve();
+    cout << fixed << setprecision(15);
+
+    return 0;
+}
