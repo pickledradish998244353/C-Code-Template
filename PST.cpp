7b8b287a458b6832e23eaefe45c72af4f82bead1
pickledradish998244353 <1249046060@qq.com>
2026-04-13 13:40:39 +0800

    主席树

---
+++ PST.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+
+#define x first
+#define y second
+#define all(x) x.begin(), x.end()
+#define vec1(T, name, n, val) vector<T> name(n, val)
+#define vec2(T, name, n, m, val) vector<vector<T>> name(n, vector<T>(m, val))
+#define vec3(T, name, n, m, k, val) vector<vector<vector<T>>> name(n, vector<vector<T>>(m, vector<T>(k, val)))
+#define vec4(T, name, n, m, k, p, val) vector<vector<vector<vector<T>>>> name((n), vector<vector<vector<T>>>((m), vector<vector<T>>((k), vector<T>((p), (val)))))
+
+using namespace std;
+using i128 = __int128;
+using u128 = unsigned __int128;
+using LL = long long;
+using LD = long double;
+using ULL = unsigned long long;
+using PII = pair<int, int>;
+using PLL = pair<LL, LL>;
+using PLD = pair<LD, LD>;
+
+const int N = 1e5 + 10, MOD = 998244353;
+const int INF = 1e9;
+const LL LL_INF = 2e18;
+const LD EPS = 1e-8;
+const int dx4[] = {-1, 0, 1, 0}, dy4[] = {0, 1, 0, -1};
+const int dx8[] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy8[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+istream& operator>>(istream& is, i128& val) {
+    string str;
+    is >> str;
+    val = 0;
+    bool flag = false;
+    if (str[0] == '-') flag = true, str = str.substr(1);
+    for (char& c : str) val = val * 10 + c - '0';
+    if (flag) val = -val;
+    return is;
+}
+
+ostream& operator<<(ostream& os, i128 val) {
+    if (val < 0) os << "-", val = -val;
+    if (val > 9) os << val / 10;
+    os << static_cast<char>(val % 10 + '0');
+    return os;
+}
+
+bool cmp(LD a, LD b) {
+    if (fabs(a - b) < EPS) return 1;
+    return 0;
+}
+
+LL qpow(LL a, LL b) {
+    LL ans = 1;
+    a %= MOD;
+    while (b) {
+        if (b & 1) ans = ans * a % MOD;
+        a = a * a % MOD;
+        b >>= 1;
+    }
+    return ans;
+}
+
+struct PST {
+    int idx;
+    vector<int> rt;
+    struct Node {
+        int ls, rs;
+        LL sm;
+        Node() {
+            ls = rs = sm = 0;
+        }
+    };
+    vector<Node> tr;
+
+    PST(int n, int m, int q) {
+        idx = 0;
+        int len = max(n, q);
+        rt.assign(len + 1, 0);
+        tr.resize(q * 40 + 10);
+    }
+
+    int clone(int p) {
+        idx++;
+        tr[idx] = tr[p];
+        return idx;
+    }
+
+    LL query(int u, int l, int r, int ql, int qr) {
+        if (!u) return 0;
+        if (l >= ql && r <= qr) return tr[u].sm;
+        int mid = l + r >> 1;
+
+        LL res = 0;
+        if (ql <= mid) res += query(tr[u].ls, l, mid, ql, qr);
+        if (qr > mid) res += query(tr[u].rs, mid + 1, r, ql, qr);
+        return res;
+    }
+
+    int modify(int u, int l, int r, int pos, int v) {
+        int nw = clone(u);
+        if (l == r) {
+            tr[nw].sm = v;
+            return nw;
+        }
+        int mid = l + r >> 1;
+        if (pos <= mid) tr[nw].ls = modify(tr[u].ls, l, mid, pos, v);
+        if (pos > mid) tr[nw].rs = modify(tr[u].rs, mid + 1, r, pos, v);
+        tr[nw].sm = tr[tr[nw].ls].sm + tr[tr[nw].rs].sm;
+        return nw;
+    }
+};
+
+void solve() {
+/**/ #ifdef LOCAL
+    cout << flush;
+/**/ #endif
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    int T = 1;
+    while (T--) solve();
+    cout << fixed << setprecision(15);
+
+    return 0;
+}
