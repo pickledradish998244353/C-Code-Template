d4cf45216c82ab3c9dd4b52e60782e8dc5c6e194
pickledradish998244353 <1249046060@qq.com>
2026-06-12 13:40:28 +0800

    更新

--- multi_test.cpp
+++ multi_test.cpp
@@ -3,6 +3,10 @@
 #define x first
 #define y second
 #define all(x) x.begin(), x.end()
+#define vec1(T, name, n, val) vector<T> name(n, val)
+#define vec2(T, name, n, m, val) vector<vector<T>> name(n, vector<T>(m, val))
+#define vec3(T, name, n, m, k, val) vector<vector<vector<T>>> name(n, vector<vector<T>>(m, vector<T>(k, val)))
+#define vec4(T, name, n, m, k, p, val) vector<vector<vector<vector<T>>>> name((n), vector<vector<vector<T>>>((m), vector<vector<T>>((k), vector<T>((p), (val)))))
 
 using namespace std;
 using i128 = __int128;
